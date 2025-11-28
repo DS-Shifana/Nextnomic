41517f41b0d2a0ab669678c9a0984efae91f5770
DS-Shifana <shifanashifa471@gmail.com>
2025-11-28 12:07:44 +0530

    review tittle updated to -get free amazon product review

--- nnform/settings.py
+++ nnform/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = 'django-insecure-fges1rg$lpor#!q*^6!l^b+tmwq^kfn22_dgctbjdj7#^kh232'
 DEBUG = True
-ALLOWED_HOSTS = ['nextnomic.pythonanywhere.com']
+ALLOWED_HOSTS = ['nextnomic.pythonanywhere.com', '127.0.0.1', 'localhost']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
