7b2cc1e0df77eb6b494c6cf3953686021d1f0855
DS-Shifana <shifanashifa471@gmail.com>
2025-11-29 09:39:47 +0530

    updated url

--- nnform/urls.py
+++ nnform/urls.py
@@ -19,6 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('adform.urls')),
+    path('adform', include('adform.urls')),
     path('reviewform/', include('reviewform.urls')),
 ]
